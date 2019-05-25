c4ee1782c419c0113ddb6b278b064324a1e8fa21
wuhao <wuhao.code@foxmail.com>
2019-05-25 20:28:37 +0800

    some change

--- WHLatticeDemo/WHLatticeDemo/AppDelegate.m
+++ WHLatticeDemo/WHLatticeDemo/AppDelegate.m
@@ -12,15 +12,10 @@
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    
     _window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
-    
     _window.backgroundColor = [UIColor whiteColor];
-    
     _window.rootViewController = [ViewController new];
-    
     [_window makeKeyAndVisible];
-    
     return YES;
 }
 
--- WHLatticeDemo/WHLatticeDemo/ViewController.m
+++ WHLatticeDemo/WHLatticeDemo/ViewController.m
@@ -8,60 +8,41 @@
 
 #import "ViewController.h"
 
-// 宽度(自定义)
 #define PIC_WIDTH 70
-// 高度(自定义)
 #define PIC_HEIGHT 80
-// 列数(自定义)
 #define COL_COUNT 4
 
 @implementation ViewController {
-    NSArray<NSString *> *pictures;
+    NSArray<NSString *> *_pictures;
 }
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
     [self loadImage];
     [self addPictures];
 }
 
-/** 加载图片(本地) */
 - (void)loadImage {
     NSMutableArray *picArray = [NSMutableArray array];
     for (int i = 0; i < 16; i++) {
         NSString *imageName = [NSString stringWithFormat:@"test_%d",i];
         [picArray addObject:imageName];
     }
-    pictures = picArray.copy;
+    _pictures = picArray.copy;
 }
 
-/** 九宫格形式添加图片 */
 - (void)addPictures {
-    
-    // 在for循环中添加
-    // pictures.count中的pictures是一个图片数组，代表着要添加多少个图片
-    for (int i = 0; i < pictures.count; i++) {
-        //创建图片
+    [_pictures enumerateObjectsUsingBlock:^(NSString * _Nonnull imageName, NSUInteger i, BOOL * _Nonnull stop) {
         UIImageView *imageView = [[UIImageView alloc] init];
-        imageView.image = [UIImage imageNamed:pictures[i]];
-        
-        // 图片所在行
+        imageView.image = [UIImage imageNamed:imageName];
         NSInteger row = i / COL_COUNT;
-        // 图片所在列
         NSInteger col = i % COL_COUNT;
-        // 间距
         CGFloat margin = (self.view.bounds.size.width - (PIC_WIDTH * COL_COUNT)) / (COL_COUNT + 1);
-        // PointX
         CGFloat picX = margin + (PIC_WIDTH + margin) * col;
-        // PointY
         CGFloat picY = margin + (PIC_HEIGHT + margin) * row;
-        
-        // 图片的frame
         imageView.frame = CGRectMake(picX, picY, PIC_WIDTH, PIC_HEIGHT);
-        
         [self.view addSubview:imageView];
-    }
+    }];
 }
 
 @end
